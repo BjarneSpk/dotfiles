879ed9fab7ddb1ee4a1132fb636f77421d593840
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2024-07-26 13:02:16 +0200

    refactored plugin config

--- nvim/lua/config/plugins/init.lua
+++ nvim/lua/config/plugins/init.lua
@@ -1,47 +1,7 @@
 return {
-  {
-	  'nvim-telescope/telescope.nvim', 
-      version = '0.1.6',
-	  dependencies = { 
-          {'nvim-lua/plenary.nvim'},
-          {'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font},
-      }
-  },
-  { 'numToStr/Comment.nvim', opts = {} },
-  {
-      'nvim-treesitter/nvim-treesitter',
-      build = ':TSUpdate'
-  },
-  'nvim-lua/plenary.nvim',
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { {'nvim-lua/plenary.nvim'} }
-  },
-  'mbbill/undotree',
+    { 'numToStr/Comment.nvim', opts = {} },
 
-  'neovim/nvim-lspconfig',
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'hrsh7th/nvim-cmp', 
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'saadparwaiz1/cmp_luasnip',
-  {
-      "L3MON4D3/LuaSnip",
-      dependencies = { "rafamadriz/friendly-snippets" },
-  },
-  'rafamadriz/friendly-snippets',
+    'nvim-lua/plenary.nvim',
 
-  {
-      "NeogitOrg/neogit",
-      dependencies = {
-          "nvim-lua/plenary.nvim",         -- required
-          "sindrets/diffview.nvim",        -- optional - Diff integration
-
-          "nvim-telescope/telescope.nvim", 
-      },
-      config = true
-  }
+    { "nvim-tree/nvim-web-devicons", lazy = true },
 }
