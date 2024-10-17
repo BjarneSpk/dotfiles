5257c1c41c23e2740e0328ddb06fc2a43a2655d9
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2024-10-17 16:47:28 +0200

    vimf and openf now no longer open applications when no file is selected

--- aliases.zsh
+++ aliases.zsh
@@ -1,10 +1,23 @@
 alias vim="nvim"
 alias py="python3"
-alias vimf='nvim $(fzf --algo=v1 --preview="bat --theme=ansi --color=always --style=numbers --line-range=:500 {}")'
-alias openf='open $(fzf --algo=v1 --preview="bat --theme=ansi --color=always --style=numbers --line-range=:500 {}")'
 alias mv="mv -i"
 alias cp="cp -i"
 alias rm="trash"
+alias server="ssh bjarne@188.245.73.235"
+
+vimf() {
+    local file=$(fzf --algo=v1 --preview="bat --theme=ansi --color=always --style=numbers --line-range=:500 {}")
+    if [[ -n "$file" ]]; then
+        nvim "$file"
+    fi
+}
+
+openf() {
+    local file=$(fzf --algo=v1 --preview="bat --theme=ansi --color=always --style=numbers --line-range=:500 {}")
+    if [[ -n "$file" ]]; then
+        open "$file"
+    fi
+}
 
 mcd() {
     mkdir "${1}" && cd "${1}"
