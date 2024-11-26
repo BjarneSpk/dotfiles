73f642d29e7cec3a6dadad6288d6d737c0915a37
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2024-11-27 00:16:26 +0100

    migrated to lazyvim 13

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,67 +1,62 @@
 return {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-        opts.config.center = {
-            {
-                action = "lua LazyVim.pick()()",
-                desc = " Find file",
-                icon = " ",
-                key = "f",
-                key_format = " %s",
-            },
-            {
-                action = "ene | startinsert",
-                desc = " New file",
-                icon = " ",
-                key = "n",
-                key_format = " %s",
-            },
-            {
-                action = 'lua LazyVim.pick("oldfiles")()',
-                desc = " Recent files",
-                icon = " ",
-                key = "r",
-                key_format = " %s",
-            },
-            {
-                action = 'lua LazyVim.pick("live_grep")()',
-                desc = " Find text",
-                icon = " ",
-                key = "g",
-                key_format = " %s",
-            },
-            {
-                action = "lua LazyVim.pick.config_files()()",
-                desc = " Config",
-                icon = " ",
-                key = "c",
-                key_format = " %s",
-            },
-            {
-                action = "LazyExtras",
-                desc = " Lazy Extras",
-                icon = " ",
-                key = "x",
-                key_format = " %s",
-            },
-            {
-                action = "Lazy",
-                desc = " Lazy",
-                icon = "󰒲 ",
-                key = "l",
-                key_format = " %s",
-            },
-            {
-                action = function()
-                    vim.api.nvim_input("<cmd>qa<cr>")
-                end,
-                desc = " Quit",
-                icon = " ",
-                key = "q",
-                key_format = " %s",
-            },
-        }
-        local logo = [[
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+        dashboard = {
+            preset = {
+                keys = {
+                    {
+                        action = "lua LazyVim.pick()()",
+                        desc = " Find file",
+                        icon = " ",
+                        key = "f",
+                    },
+                    {
+                        action = "ene | startinsert",
+                        desc = " New file",
+                        icon = " ",
+                        key = "n",
+                    },
+                    {
+                        action = 'lua LazyVim.pick("oldfiles")()',
+                        desc = " Recent files",
+                        icon = " ",
+                        key = "r",
+                    },
+                    {
+                        action = 'lua LazyVim.pick("live_grep")()',
+                        desc = " Find text",
+                        icon = " ",
+                        key = "g",
+                    },
+                    {
+                        action = "lua LazyVim.pick.config_files()()",
+                        desc = " Config",
+                        icon = " ",
+                        key = "c",
+                    },
+                    {
+                        action = "LazyExtras",
+                        desc = " Lazy Extras",
+                        icon = " ",
+                        key = "x",
+                    },
+                    {
+                        action = "Lazy",
+                        desc = " Lazy",
+                        icon = "󰒲 ",
+                        key = "l",
+                    },
+                    {
+                        action = function()
+                            vim.api.nvim_input("<cmd>qa<cr>")
+                        end,
+                        desc = " Quit",
+                        icon = " ",
+                        key = "q",
+                    },
+                },
+                header = [[
                                                                     
       ████ ██████           █████      ██                     
      ███████████             █████                             
@@ -70,9 +65,8 @@ return {
    █████████ ██████████ █████████ █████ █████ ████ █████   
  ███████████ ███    ███ █████████ █████ █████ ████ █████  
 ██████  █████████████████████ ████ █████ █████ ████ ██████ 
-]]
-
-        logo = string.rep("\n", 8) .. logo .. "\n\n"
-        opts.config.header = vim.split(logo, "\n")
-    end,
+]],
+            },
+        },
+    },
 }
