dab041b5ed2fee8ee256346dea8361d58f06e6a3
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2024-12-14 22:41:02 +0100

    removed äö keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -24,39 +24,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 vim.keymap.set("n", "<leader>/", "<C-W>v", { desc = "Split Window Right", remap = true })
 
--- change [] to öä
-local diagnostic_goto = function(next, severity)
-    local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-    severity = severity and vim.diagnostic.severity[severity] or nil
-    return function()
-        go({ severity = severity })
-    end
-end
-
 vim.keymap.del({ "i", "x", "n", "v" }, "<C-s>")
-
-vim.keymap.del("n", "]q")
-vim.keymap.del("n", "[q")
-
-vim.keymap.del("n", "[d")
-vim.keymap.del("n", "]d")
-vim.keymap.del("n", "[e")
-vim.keymap.del("n", "]e")
-vim.keymap.del("n", "[w")
-vim.keymap.del("n", "]w")
-
-vim.keymap.del("n", "<leader><tab>[")
-vim.keymap.del("n", "<leader><tab>]")
-
-vim.keymap.set("n", "öq", vim.cmd.cprev, { desc = "Previous Quickfix" })
-vim.keymap.set("n", "äq", vim.cmd.cnext, { desc = "Next Quickfix" })
-
-vim.keymap.set("n", "äd", diagnostic_goto(true), { desc = "Next Diagnostic" })
-vim.keymap.set("n", "öd", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-vim.keymap.set("n", "äe", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-vim.keymap.set("n", "öe", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-vim.keymap.set("n", "äw", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-vim.keymap.set("n", "öw", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
-vim.keymap.set("n", "<leader><tab>ä", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>ö", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
