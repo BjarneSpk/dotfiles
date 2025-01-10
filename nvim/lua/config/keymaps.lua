7c5b32f224c6b4e590e01310e48379f7d77b037a
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2025-01-10 23:47:07 +0100

    kitty scrollback

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-vim.keymap.set("n", "<leader><cr>", "a<CR><Esc>")
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Block Up" })
 
@@ -23,5 +21,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 vim.keymap.set("n", "<leader>/", "<C-W>v", { desc = "Split Window Right", remap = true })
-
-vim.keymap.del({ "i", "x", "n", "v" }, "<C-s>")
