a62e9e8b4d8dfd95ba2ade63354c56624985e5e1
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2025-05-05 09:35:06 +0200

    improved link script and adjusted link targets to XDG_CONFIG_HOME

--- link.sh
+++ link.sh
@@ -1,39 +1,54 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-backup_and_link() {
+link() {
   local target=$1
   local link=$2
 
-  # local parent_dir=$(dirname $link)
-  # if [[ ! -d $parent_dir ]]; then
-  #   echo "Creating directory $parent_dir"
-  #   mkdir -p $parent_dir
-  # fi
-
-  if [[ -e $link || -L $link ]]; then
-    echo "Backing up existing $link to ${link}.bak"
-    mv $link ${link}.bak
+  local parent_dir=$(dirname $link)
+  if [[ ! -d $parent_dir ]]; then
+    mkdir -p $parent_dir
   fi
 
-  echo "Creating symbolic link for $target at $link"
-  ln -s "$target" "$link"
+  if [[ -e $link || -L $link ]]; then
+    print -P "%F{yellow}Warning:%f Link target already exists: ${link}"
+    print -P "Choose an option:"
+    print -P "1) Replace (backup old file to ${link}.bak)"
+    print -P "2) Skip this link"
+    print -P "3) Abort operation"
+    while true; do
+      read -k "choice?Your choice [1/2/3]: "
+      case $choice in
+        1)
+          print "\nBacking up existing file and creating new link"
+          mv "$link" "${link}.bak"
+          ln -sf "$target" "$link"
+          return 0
+          ;;
+        2)
+          print "\nSkipping this link"
+          return 0
+          ;;
+        3)
+          print "\nAborting operation"
+          return 1
+          ;;
+        *)
+          print "\nInvalid choice, please try again"
+          ;;
+      esac
+    done
+  else
+    ln -s "$target" "$link"
+  fi
 }
 
-# Associative array of source to destination mappings
-declare -A files=(
-  ["~/dotfiles/zsh/.zshrc"]="~/.zshrc"
-  ["~/dotfiles/zsh/.zshenv"]="~/.zshenv"
-  ["~/dotfiles/zsh/.zprofile"]="~/.zprofile"
-  ["~/dotfiles/.gitconfig"]="~/.gitconfig"
-  ["~/dotfiles/nvim"]="~/.config/nvim"
-  ["~/dotfiles/tmux.conf"]="~/.config/tmux/tmux.conf"
-  ["~/dotfiles/yazi"]="~/.config/yazi"
-  ["~/dotfiles/kitty"]="~/.config/kitty"
-  ["~/dotfiles/bat"]="~/.config/bat"
-  ["~/dotfiles/aerospace"]="~/.config/aerospace"
-)
-
-# Iterate through the array and create symlinks
-for target link in "${(@kv)files}"; do
-  backup_and_link $~target $~link
-done
+link "${DOTFILES}/zsh/zshrc" "${XDG_CONFIG_HOME}/zsh/.zshrc"
+link "${DOTFILES}/zsh/zprofile" "${XDG_CONFIG_HOME}/zsh/.zprofile"
+link "${DOTFILES}/zsh/zshenv" "${HOME}/.zshenv"
+link "${DOTFILES}/git/config" "${XDG_CONFIG_HOME}/git/config"
+link "${DOTFILES}/nvim" "${XDG_CONFIG_HOME}/nvim"
+link "${DOTFILES}/tmux.conf" "${XDG_CONFIG_HOME}/tmux/tmux.conf"
+link "${DOTFILES}/yazi" "${XDG_CONFIG_HOME}/yazi"
+link "${DOTFILES}/kitty" "${XDG_CONFIG_HOME}/kitty"
+link "${DOTFILES}/bat" "${XDG_CONFIG_HOME}/bat"
+link "${DOTFILES}/aerospace" "${XDG_CONFIG_HOME}/aerospace"
