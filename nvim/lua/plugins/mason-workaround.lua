ba4b55c3a6eff10a6151e52f05824dfd287686be
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2025-05-11 23:00:21 +0200

    nvim workaround, pin mason to old version to prevent breaking changes

---
+++ nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+# Mason major version bump broke something, so pin to old version.
+return {
+    { "mason-org/mason.nvim", version = "^1.0.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
