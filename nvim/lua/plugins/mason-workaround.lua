cde97310a8c8c76f6834ed125a8a3dd573112ee4
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2025-05-30 22:58:31 +0200

    remove mason workaround
    fix was added to LazyVim, so now redundant

--- nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,5 +0,0 @@
-# Mason major version bump broke something, so pin to old version.
-return {
-    { "mason-org/mason.nvim", version = "^1.0.0" },
-    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
