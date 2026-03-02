4903ec96d5c2d158e1e51ddc453e5645db3f9feb
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2026-03-02 20:17:16 +0100

    feat(hypr): make bitwarden popup float dynamically
    firefox renames the window so windowrule doesn't apply :/

--- scripts/hyprext.sh
+++ scripts/hyprext.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+timeout=0
 handle() {
   case $1 in
     monitoradded\>* | monitorremoved\>*) brightness.sh redetect ;;
+    windowtitlev2*)
+        # Extract the window ID from the line
+        window_id=${1#*>>}
+
+        # Fetch the list of windows and parse it using jq to find the window by its decimal ID
+        window_info=$(hyprctl clients -j | jq --arg id "0x$window_id" '.[] | select(.address == ($id))')
+
+        # Extract the title from the window info
+        window_title=$(echo "$window_info" | jq -r '.title')
+
+        # Check if the title matches the characteristics of the Bitwarden popup window
+        if [[ "$window_title" == *"(Bitwarden"*"Password Manager) - Bitwarden"* ]]; then
+
+            # in case of double rename only allow once per second
+            if ((SECONDS < timeout)); then
+                return
+            fi
+            timeout=$((SECONDS + 1))
+
+            hyprctl --batch "dispatch togglefloating address:0x$window_id; dispatch resizewindowpixel exact 20% 54%,address:0x$window_id; dispatch centerwindow"
+        fi
+        ;;
   esac
 }
 
