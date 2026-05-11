3b70b410f00dd9807175f150d5e3e511e0f9d12a
Bjarne <129185929+BjarneSpk@users.noreply.github.com>
2026-05-11 21:31:49 +0200

    fix(wpchanger): correclty set file paths for every monitor

--- scripts/wpchanger.sh
+++ scripts/wpchanger.sh
@@ -67,11 +67,14 @@ ln -sf "$img_path" "$cache_dir/current"
 
 awww img "$cache_dir/current" -t random --transition-pos 0.5,0.5 --transition-fps 60 --transition-duration 1
 
-template_file=$(find "$XDG_CACHE_HOME/awww" -name "$(awww query | awk '{print $2}' | tr -d ':')")
-for name in $(ls /sys/class/drm/ | grep -oP 'card\d+-\K.*' | awk '{print $1}'); do
-    [ "$name" = "$(basename $template_file)" ] && continue
-    cp "$template_file" "$(dirname $template_file)/$name"
-done
+active_output="$(awww query | awk 'NR==1 {print $1}' | tr -d ':')"
+template_file="$(find "$XDG_CACHE_HOME/awww" -type f -name "$active_output" -print -quit)"
+if [[ -n "$template_file" ]]; then
+    for name in $(ls /sys/class/drm/ | grep -oP 'card\d+-\K.*' | awk '{print $1}'); do
+        [ "$name" = "$(basename "$template_file")" ] && continue
+        cp "$template_file" "$(dirname "$template_file")/$name"
+    done
+fi
 
 matugen --quiet image "$(readlink -f "$img_path")" --mode dark --source-color-index 0 &
 
